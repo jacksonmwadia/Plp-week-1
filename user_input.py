492ff86be6c9cc51cad24fca7658c946a5f21cce
jacksonmwadia <mwadiajackson@gmail.com>
2024-10-11 14:59:16 +0300

    first commit

---
+++ user_input.py
@@ -0,0 +1,9 @@
+# user_input.py
+
+# Asking for user input
+name = input("Please enter your name: ")
+age = input("Please enter your age: ")
+location = input("Please enter your location: ")
+
+# Output a personalized message
+print(f"Hello {name}, you are {age} years old and live in {location}.")
